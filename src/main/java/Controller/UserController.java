1c8ebd15b97fbff8551ca427318264700328adee
guptasonu14 <sguptabudha@gmail.com>
2022-11-10 18:54:25 +0530

    rewrite something

--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -1,12 +1,25 @@
 package Controller;
 
+import model.Post;
 import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import service.PostService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
+
+    public UserController(PostService postService) {
+        this.postService = postService;
+    }
 
     @RequestMapping("users/login")
     public String login() {
@@ -18,9 +31,15 @@ public class UserController {
         return "users/registration";
     }
     @RequestMapping(value = "users/login", method = RequestMethod.POST)
-    public String loginUser(User user){
+    public String loginUser(User user) {
         return "redirect:/posts";
+    }
+    @RequestMapping(value = "users/logout", method = RequestMethod.POST)
+        public String Logout(Model model){
+        ArrayList<Post> posts = postService.getAllPosts();
 
+        model.addAttribute("posts",posts);
+        return "index";
 
     }
 }
